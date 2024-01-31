8519023ff8d5c8394d2db85c44554b36ba2f3cab
yuvraj rajput <114816380+yuvii6264@users.noreply.github.com>
2024-01-31 21:54:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
-import { useEffect, useState } from 'react'
+import {  useState } from 'react'
 import PrivateRoute from "./components/PrivateRoute";
 
 
